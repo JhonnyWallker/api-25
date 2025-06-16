08ba9aef60f944266c3dfd87fad0b4dab775a005
simon <blfernando.fb@gmail.com>
2025-06-16 15:30:36 -0300

    primer commit

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import noticia from './noticia'
+import publicidad from './publicidad'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, noticia, publicidad]
